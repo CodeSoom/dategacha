28c5de2ee5d063b5dd39bbe65618e3bd3b0697b3
Seungmin Lee <seungmin@uxplus.dev>
2020-11-30 00:51:21 +0900

    Configure `webpack` and "deploy" command to deploy project
    - Modify `package.json` to add "deploy" command for gh-pages.
    - Modify `webpack.config.js` to set output file name.
    - Build new files.
    - Add `index.html`.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './main.js',
+  },
   module: {
     rules: [
       {
