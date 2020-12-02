79d1df4bbbe71d730c63fb1e1e1d07d38c2bc863
Seungmin Lee <seungmin@uxplus.dev>
2020-12-02 11:45:02 +0900

    Show a random date idea when the button is clicked
    - Add a button to press.
    - Add a paragraph to show random idea.
    - Add `handleClick` to generate a random date idea.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import ideas from '../fixtures/ideas';
+
+function handleClick(event) {
+  event.preventDefault();
+
+  document.querySelector('[data-testid="idea"]').innerHTML = ideas[Math.floor(Math.random() * 30)].description;
+}
+
 export default function App() {
   return (
     <>
-      <p>뉴노멀 시대를 살아가는 우리의 추억 생성기</p>
+      <p>뉴노멀 시대를 살아가는 우리의 추억 생성기, 데잇가챠</p>
+      <button type="button" onClick={handleClick}>데이트 아이디어 얻기</button>
+      <p data-testid="idea" />
     </>
   );
 }
